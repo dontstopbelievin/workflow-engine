b3ade9ad2e52262e1f11cc54057f06f67d1a792f
Sultankhan Tursunov <tursunov.sultankhan@gmail.com>
2020-09-14 15:16:09 +0600

    iin/bin fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('iin')->nullable();
+            $table->string('bin')->nullable();
             $table->string('phone')->nullable();
             $table->string('usertype')->nullable();
             $table->string('email')->unique();
