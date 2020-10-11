5aea8eeaaeb4990d04ee6421dd8ae9f38ee16bd7
Askar <askaraitymov@gmail.com>
2020-10-11 16:29:38 +0600

    shep integration: geoportal egkn receive layer

--- app/Auction.php
+++ app/Auction.php
@@ -12,85 +12,102 @@ class Auction extends Model
     {
         return array(
             'Request' => [
-                'Auction' => [
-                    'LotID' => $this->lot_id,
-                    'EgknID' => $this->egkn_id,
-                    'StatusZU' => $this->status_zu, // "free" - свободный ЗУ; "auction" - аукцион/торги
-                    'LotNumber' => $this->lot_number,
-                    'LotStatus' => $this->lot_status, //"1" - Предстоящий; "2" - Несостоявшийся; "3" - Состоявшийся
-                    'Target' => [
-                        'ID' => $this->target,
-                        'NameRus' => $this->target,
-                        'NameKaz' => $this->target
-                    ],
-                    'Purpose' => [
-                        'ID' => $this->purpose,
-                        'NameRus' => $this->purpose,
-                        'NameKaz' => $this->purpose
-                    ],
-                    'RightType' => [
-                        'ID' => '',
-                        'NameRus' => $this->right_type,
-                        'NameKaz' => $this->right_type
-                    ],
-                    'RentConditionsRus' => $this->rent_conditions_rus,
-                    'RentConditionsKaz' => $this->rent_conditions_kaz,
-                    'Area' => $this->area,
-                    'CadastreCost' => $this->cadastre_cost,
-                    'StartCost' => $this->start_cost,
-                    'TaxCost' => $this->tax_cost,
-                    'ParticipationCost' => $this->participation_cost,
-                    'AuctionMethod' => $this->auction_method, //"1" - английский; "2" - голландский
-                    'AuctionDate' => $this->auction_date_time,
-                    'AuctionPlaceRus' => $this->auction_place_rus,
-                    'AuctionPlaceKaz' => $this->auction_place_kaz,
-                    'RequestAddressRus' => $this->request_address_rus,
-                    'RequestAddressKaz' => $this->request_address_kaz,
-                    'CommentRus' => $this->comment_rus,
-                    'CommentKaz' => $this->comment_kaz,
-                    'Seller' => [
-                        'IINBIN' => $this->iin_bin,
-                        'NameRus' => $this->name_rus,
-                        'NameKaz' => $this->name_kaz
-                    ],
-                    'Customer' => [
-                        'IINBIN' => $this->iin_bin,
-                        'NameRus' => $this->name_rus,
-                        'NameKaz' => $this->name_kaz
-                    ],
-                    'AddressRus' => $this->address_rus,
-                    'AddressKaz' => $this->address_kaz,
-                    'PublishDate' => $this->publish_date,
-                    'Files' => [
-                        'FileName' => $this->identification_doc, //with extension
-                        'FileData' => $this->identification_doc
-                    ],
-                    'Coordinates' => $this->coordinates_1,
-                    'CoordinateSystem' => $this->coordinate_system,
-                    'AteID' => $this->ate_id,
-                    'TechConditions' => [
-                        'ElektrPower' => $this->elektr_power,
-                        'ElektrFaza1' => $this->elektr_faza_1,
-                        'ElektrFaza3' => $this->elektr_faza_3,
-                        'WaterPower' => $this->water_power,
-                        'WaterHoz' => $this->water_hoz,
-                        'WaterProduction' => $this->water_production,
-                        'SeweragePower' => $this->sewerage_power,
-                        'SewerageFecal' => $this->sewerage_fecal,
-                        'SewerageProduction' => $this->sewerage_production,
-                        'SewerageClean' => $this->sewerage_clean,
-                        'HeatPower' => $this->heat_power,
-                        'HeatFiring' => $this->heat_firing,
-                        'HeatVentilation' => $this->heat_ventilation,
-                        'HeatHotWater' => $this->heat_hot_water,
-                        'StormWater' => $this->storm_water,
-                        'Telekom' => $this->telekom,
-                        'GasPower' => $this->gas_power,
-                        'GasOnCooking' => $this->gas_on_cooking,
-                        'GasHeating' => $this->gas_heating,
-                        'GasVentilation' => $this->gas_ventilation,
-                        'GasConditioning' => $this->gas_conditioning,
-                        'GasHotWater' => $this->gas_hot_water
+                'BusinessData' => [
+                    'LayerCode' => 'FREE_LAND',
+                    'Auction' => [
+                        'LotID' => '2404_006',
+                        'EgknID' => '',
+                        'StatusZU' => 'auction', // "free" - свободный ЗУ; "auction" - аукцион/торги
+                        'LotNumber' => '14',
+                        'LotStatus' => 1, //"1" - Предстоящий; "2" - Несостоявшийся; "3" - Состоявшийся
+                        'Target' => [
+                            'ID' => '7020100100',
+                            'NameRus' => 'Индивидуальный жилой дом',
+                            'NameKaz' => 'Жеке тұрғын үй'
+                        ],
+                        'Purpose' => [
+                            'ID' => '37',
+                            'NameRus' => 'строительство',
+                            'NameKaz' => 'құрылыс салу'
+                        ],
+                        'RightType' => [
+                            'ID' => '10',
+                            'NameRus' => 'временное возмездное долгосрочное землепользование',
+                            'NameKaz' => 'Уақытша (ұзақ уақыт) жерді пайдалану құқығы'
+                        ],
+                        'LandDivisibility' => [
+                            'ID' => 'divis_y',
+                            'NameRus' => 'Частная собственность',
+                            'NameKaz' => 'Жеке мулик'
+                        ],
+                        'RentConditionsRus' => 'Право аренды сроком на 3 года',
+                        'RentConditionsKaz' => 'Право аренды сроком на 3 года',
+                        'RentLease' => '2022-07-30',
+                        'Area' => '0.98',
+                        'CadastreCost' => '4850000.0',
+                        'StartCost' => '291000.0',
+                        'TaxCost' => '101300.0',
+                        'ParticipationCost' => '115450.0',
+                        'AuctionMethod' => '1', //"1" - английский; "2" - голландский
+                        'AuctionDate' => '2020-10-12T17:17:25.497+06:00',
+                        'AuctionPlaceRus' => 'Акимат города Астана',
+                        'AuctionPlaceKaz' => 'Астана қаласының әкімдігі',
+                        'RequestAddressRus' => 'Улица Алтынсарина, 2',
+                        'RequestAddressKaz' => 'Алтынсарина көшесі, 2',
+                        'CommentRus' => 'Акт согласован',
+                        'CommentKaz' => 'Акт согласован',
+                        'Seller' => [
+                            'IINBIN' => '020340002753',
+                            'IsFl' => 'false',
+                            'NameRus' => 'ГУ АППАРАТ АКИМА ГОРОДА НУР-СУЛТАН',
+                            'NameKaz' => 'ГУ АППАРАТ АКИМА ГОРОДА НУР-СУЛТАН (каз.яз.)'
+                        ],
+                        'Customer' => [
+                            'IINBIN' => '900319350069',
+                            'IsFl' => 'true',
+                            'NameRus' => 'Иванов Иван Иванович',
+                            'NameKaz' => 'Иванов Иван Иванович'
+                        ],
+                        'AddressRus' => 'Астана, мкр. Промышленный',
+                        'AddressKaz' => 'Астана қ., Промышленный ауд.',
+                        'PublishDate' => '2020-10-10',
+//                        'Files' => [
+//                            'FileName' => $this->identification_doc, //with extension
+//                            'FileData' => $this->identification_doc
+//                        ],
+                        'Coordinates' => 'MULTIPOLYGON(((5180471.87897782 7195642.55024562,5180481.43360635 7195986.5168729,5180963.9423474 7195943.52104449,5180973.49697594 7195671.21413122,5180471.87897782 7195642.55024562)))',
+                        'CoordinateSystem' => '3857',
+                        'AteID' => '68402',
+                        'NoteRus' => 'примечание рус',
+                        'NoteKaz' => 'примечание каз',
+                        'RestrictionsAndBurdensRus' => 'ограничений нет',
+                        'RestrictionsAndBurdensKaz' => 'ограничений нет',
+                        'InstalmentSelling' => '1',
+                        'InstallmentPeriod' => '12',
+                        'TechConditions' => [
+                            'ElektrPower' => '25',
+                            'ElektrFaza1' => '111',
+                            'ElektrFaza3' => '333',
+                            'WaterPower' => '444',
+                            'WaterHoz' => '5555',
+                            'WaterProduction' => '66',
+                            'SeweragePower' => '777',
+                            'SewerageFecal' => '88',
+                            'SewerageProduction' => '999',
+                            'SewerageClean' => '1010',
+                            'HeatPower' => '11',
+                            'HeatFiring' => '12',
+                            'HeatVentilation' => '1313',
+                            'HeatHotWater' => '14140',
+                            'StormWater' => 'Есть',
+                            'Telekom' => 'Есть',
+                            'GasPower' => '1515',
+                            'GasOnCooking' => '1616',
+                            'GasHeating' => '171',
+                            'GasVentilation' => '1818',
+                            'GasConditioning' => '19',
+                            'GasHotWater' => '2020'
+                        ]
                     ]
                 ]
             ],
