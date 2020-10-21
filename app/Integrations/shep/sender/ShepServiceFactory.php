8d45b0693b630eaa779021a689982807bafa603c
Askar <askaraitymov@gmail.com>
2020-10-21 22:23:47 +0600

    shep integration: egkn geoportal actualization

--- app/Integrations/shep/sender/ShepServiceFactory.php
+++ app/Integrations/shep/sender/ShepServiceFactory.php
@@ -6,6 +6,7 @@ use App\Integrations\shep\sender\services\EdsTempFilesService;
 use App\Integrations\shep\sender\services\EgknUniversalReceiveOrderService;
 use App\Integrations\shep\sender\services\EgknUniversalReceiveStatusService;
 use App\Integrations\shep\sender\services\GeoportalEgknReceiveLayerService;
+use App\Integrations\shep\sender\services\EgknGeoportalActualizationService;
 
 class ShepServiceFactory
 {
@@ -18,6 +19,8 @@ class ShepServiceFactory
                 return new EgknUniversalReceiveOrderService();
             case 'geoportal_egkn_receive_layer':
                 return new GeoportalEgknReceiveLayerService();
+            case 'egkn_geoportal_actualization':
+                return new EgknGeoportalActualizationService();
             case 'eds_temp_files':
                 return new EdsTempFilesService();
             default:
