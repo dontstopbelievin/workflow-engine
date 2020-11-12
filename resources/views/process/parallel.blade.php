6d57dc5fd99e37ab157e85a27c49c6051dae5e3f
Sultankhan Tursunov <tursunov.sultankhan@gmail.com>
2020-11-12 17:45:06 +0600

    Update parallel.blade.php

--- resources/views/process/parallel.blade.php
+++ resources/views/process/parallel.blade.php
@@ -1,26 +1,36 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Parallel Roles Creation</title>
-</head>
-<body>
-    <h1>{{$process->name}}</h1>
-    @foreach($parallelRoles as $role)
-            <h6>{{$role->name}}</h6>
-            <select name="priority" id="priority">
-                @for($i = 1; $i <= $rolesLen; $i++)
-                <option value="{{$i}}">{{$i}}</option>
-                    @endfor
-            </select>
-            <select name="role" id="role">
-                @foreach($roles as $r)
-                <option value="{{$r->name}}">{{$r->name}}</option>
-                    @endforeach
-            </select>
-        @endforeach
-</body>
-</html>
+@extends('layouts.master')
+
+@section('title')
+    Создание процесса
+@endsection
+@section('content')
+
+    <body>
+        <div class="row mx-auto">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row justify-content-center align-items-center">
+                            <div class="col-md-9">
+                                <h1>{{ $process->name }}</h1>
+                                @foreach ($parallelRoles as $role)
+                                    <h6 class="p-8">{{ $role->name }}</h6>
+                                    <select name="priority" id="priority">
+                                        @for ($i = 1; $i <= $rolesLen; $i++)
+                                            <option value="{{ $i }}">{{ $i }}</option>
+                                        @endfor
+                                    </select>
+                                    <select name="role" id="role" class="w-75">
+                                        @foreach ($roles as $r)
+                                            <option value="{{ $r->name }}">{{ $r->name }}</option>
+                                        @endforeach
+                                    </select>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+@endsection
