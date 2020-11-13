bd3de8b32d3b9a2b0b097ac45e3441515839bae1
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-11-13 09:50:32 +0600

    update parallel.blade.php

--- resources/views/process/parallel.blade.php
+++ resources/views/process/parallel.blade.php
@@ -10,17 +10,36 @@
 <body>
     <h1>{{$process->name}}</h1>
     @foreach($parallelRoles as $role)
+            {{$it = $loop->iteration}}
             <h6>{{$role->name}}</h6>
-            <select name="priority" id="priority">
+            <select id="select_id" name="priority">
                 @for($i = 1; $i <= $rolesLen; $i++)
                 <option value="{{$i}}">{{$i}}</option>
                     @endfor
             </select>
-            <select name="role" id="role">
+            <select onchange="val({{$it}})" name="role" id="role">
                 @foreach($roles as $r)
-                <option value="{{$r->name}}">{{$r->name}}</option>
+                <option id = "option" value="{{$r->name}}">{{$r->name}}</option>
                     @endforeach
             </select>
+
+            <ul id="{{$it}}">
+
+            </ul>
         @endforeach
+
+
+    <script>
+        function val(iteration) {
+            console.log(iteration)
+            // d = document.getElementById("role").value;
+            var ul = document.getElementById(iteration);
+            console.log(ul);
+            // twoIterations = iteration + iteration;
+            var li = document.createElement(li);
+            li.innerHTML = document.getElementById("role").value;
+            ul.appendChild(li);
+        }
+    </script>
 </body>
 </html>
