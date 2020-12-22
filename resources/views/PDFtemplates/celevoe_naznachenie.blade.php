a6d6a7c33ffa8c89f917a9bc83d2a6862a743f14
Sultankhan Tursunov <tursunov.sultankhan@gmail.com>
2020-12-22 15:45:11 +0600

    Update celevoe_naznachenie.blade.php

--- resources/views/PDFtemplates/celevoe_naznachenie.blade.php
+++ resources/views/PDFtemplates/celevoe_naznachenie.blade.php
@@ -7,58 +7,40 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <style type="text/css">
-    h2 {
-        text-align: center;
-        font-size: 22px;
-        margin-bottom: 50px;
-    }
-
     body {
-        background: #f2f2f2;
         font-family: times;
-    }
-
-    .section {
-        margin-top: 30px;
-        padding: 50px;
-        background: #fff;
-    }
-
-    .pdf-btn {
-        margin-top: 30px;
+        font-size: 14px;
     }
 
 </style>
 
 <body>
     <div class="container">
-        <div class="col-md-12 section offset-md-2">
+        <div class="col-md-12">
             <div class="panel panel-primary">
                 <div class="panel-heading">
-                    <img src="{{URL::asset('/images/header.jpg')}}" alt="logo" height="165"/>
+                    {{-- <img src="{{ URL::asset('/images/header.jpg') }}" alt="logo" height="185" /> --}}
+                    <p style="text-align: center;"><b>Выписка из постановления акимата города
+                            Нур-Султан</b></p>
+                    <hr style="height:1px;border-width:0; background-color:#008cff;">
+                    <table class="header_table">
+                        <tr class="header_bottom">
+                            <td width="50%">
+                                <b>№ {{ $variable }}<br /></b>
+                            </td>
+                            <td width="50%" style="horizontal-align: right;">
+                                <b>от «{{ $variable }}» {{ $variable }} года<br /></b>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
                 <div class="panel-body">
                     <div class="main-div">
-
-                        <p style="text-align: center; padding-left: 10px;"><b>Выписка из постановления акимата города
-                                Нур-Султан</b></p>
-                        <table>
-                            <tr class="header_bottom">
-                                <td>
-                                    № {{ $variable }}<br />
-                                </td>
-                                <td></td>
-                                <td style="text-align: right;">
-                                    от «{{ $variable }}» {{ $variable }} года<br />
-                                </td>
-                                <td style="text-align: left;">
-                                    Об изменении целевого
-                                    назначения земельного участка
-                                    <br />
-                                </td>
-                            </tr>
-                        </table>
-                        <div>
+                        <p style="padding-top: 15px;" width="40%">
+                            Об изменении целевого <br />
+                            назначения земельного участка
+                        </p>
+                        <p style="padding-top: 15px; text-indent: 2em;">
                             В соответствии со статьей 49-1 Земельного кодекса Республики Казахстан от 20 июня 2003 года,
                             статьей 37 Закона
                             Республики Казахстан от 23 января 2001 года «О местном государственном управлении и
@@ -67,8 +49,8 @@
                             «{{ $variable }}»
                             {{ $variable }} года №{{ $variable }},
                             акимат города Нур-Султан <b>ПОСТАНОВЛЯЕТ:</b>
-                        </div>
-                        <p style="text-indent: 5em;">
+                        </p>
+                        <p style="text-indent: 2em;">
                             1. Изменить {{ $updatedFields['cadastral_number'] }} целевое назначение земельногоучастка с
                             кадастровым номером
                             21-{{ $updatedFields['cadastral_number'] }}, площадью
@@ -77,10 +59,10 @@
                             «Строительство {{ $variable }}», расположенный по адресу: город Нур-Султан,
                             район «{{ $variable }}», ул. {{ $variable }}, участок № {{ $variable }}.
                         </p>
-                        <p style="text-indent: 5em;">
+                        <p style="text-indent: 2em;">
                             получить акт на право собственности на земельный участок;
                         </p>
-                        <p style="text-indent: 5em;">
+                        <p style="text-indent: 2em;">
                             обеспечить беспрепятственный проезд и доступ уполномоченным органам, смежным
                             землепользователям, собственникам для
                             строительства и эксплуатации подземных и надземных коммуникаций в установленном
@@ -88,10 +70,10 @@
                             Казахстан порядке.
                         </p>
 
-                        <p style="text-align: left; padding-left: 5em;">
+                        <p style="text-indent: 2em;">
                             <b>Выписка верна</b>
                         </p>
-                        <p style="text-align: left; padding-left: 5em;">
+                        <p style="padding-left: 2em; width: 200px;">
                             <b>Руководитель Управления
                                 архитектуры, градостроительства
                                 и земельных отношений
