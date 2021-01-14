790a1b2be8d596301e9dd5f89c762fd70a99c484
aman <amanbek94@gmail.com>
2021-01-14 11:21:26 +0600

    test shep

--- app/Http/Controllers/IntegrationController.php
+++ app/Http/Controllers/IntegrationController.php
@@ -43,16 +43,6 @@ class IntegrationController extends Controller
 
     public function prettyXml(){
         $xml = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://bip.bee.kz/AsyncChannel/v10/Types"><soapenv:Header xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" soapenv:mustUnderstand="1"><ds:Signature xmlns:ds="http://www.w3.org/2000/09/xmldsig#" Id="SIG-89B96E14377C3A44E2161060010050223455"><ds:SignedInfo><ds:CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/><ds:SignatureMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#gost34310-gost34311"/><ds:Reference URI="#id-89B96E14377C3A44E2161060010050223454"><ds:Transforms><ds:Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/></ds:Transforms><ds:DigestMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#gost34311"/><ds:DigestValue>8RqYmVwUsH/+3hzD4+USx+fAs4HvctrcKnqrZmUOlWM=</ds:DigestValue></ds:Reference></ds:SignedInfo><ds:SignatureValue>2lYfz1Hx/qzvxWMn6bt+m89rGwAuWKF4+/TV1WgF42K3uyk8BEMX25A8J2XZQZJH/Zh37tFx/Few6540v/v6xA==</ds:SignatureValue><ds:KeyInfo Id="KI-89B96E14377C3A44E2161060010050123452"><wsse:SecurityTokenReference wsu:Id="STR-89B96E14377C3A44E2161060010050123453"><ds:X509Data><ds:X509IssuerSerial><ds:X509IssuerName>CN=ҰЛТТЫҚ КУӘЛАНДЫРУШЫ ОРТАЛЫҚ (GOST),C=KZ</ds:X509IssuerName><ds:X509SerialNumber>417157195680026735073120748251296757333772328683</ds:X509SerialNumber></ds:X509IssuerSerial></ds:X509Data></wsse:SecurityTokenReference></ds:KeyInfo></ds:Signature></wsse:Security></soapenv:Header><soap:Body xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="id-89B96E14377C3A44E2161060010050223454"> <typ:sendMessageResponse> <response> <messageId>212198064</messageId> <correlationId>3204633</correlationId> <responseDate>2021-01-14T10:54:39.376+06:00</responseDate> <sessionId>{f2632129-2406-4736-93ae-cd4eb6c9fb5f}</sessionId> </response> </typ:sendMessageResponse></soap:Body> </soap:Envelope>';
-        XmlDocument xml = new \XmlDocument();
-        xml.LoadXml($xml); // suppose that myXmlString contains "<Names>...</Names>"
-
-        XmlNodeList xnList = xml.SelectNodes("/Names/Name");
-        foreach (XmlNode xn in xnList)
-        {
-          string firstName = xn["FirstName"].InnerText;
-          string lastName = xn["LastName"].InnerText;
-          Console.WriteLine("Name: {0} {1}", firstName, lastName);
-        }
         return ;
     }
 
