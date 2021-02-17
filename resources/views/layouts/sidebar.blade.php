e7cfd2b75474eb5b2f80c0cd6d8a471fc5a22935
Sultankhan Tursunov <tursunov.sultankhan@gmail.com>
2021-02-17 12:25:41 +0600

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,100 +2,64 @@
   <div class="scrollbar-inner sidebar-wrapper" style="padding-top: 0px;">
     @if(Auth::user()->usertype != "admin")
       <ul class="nav">
-        @if( request()->segment(1) == 'auction' )
-            <li class="nav-item active">
-        @else
-            <li class="nav-item">
-        @endif
-              <a href="{{ route('auction.index') }}">
-                  <i class="la la-legal"></i>
-                  <p>Аукцион</p>
-              </a>
+          <li class="nav-item {{request()->segment(1) == 'auction' ? 'active' : ''}}">
+            <a href="{{ route('auction.index') }}">
+                <i class="la la-legal"></i>
+                <p>Аукцион</p>
+            </a>
           </li>
-          @if (request()->segment(1) == 'egknservice')
-                <li class="nav-item active">
-                @else
-                <li class="nav-item">
-            @endif
-              <a href="{{ route('egknservice.index') }}">
-                  <i class="la la-archive"></i>
-                  <p>Поступившие заявки</p>
-              </a>
+          <li class="nav-item {{request()->segment(1) == 'egknservice' ? 'active' : ''}}">
+            <a href="{{ route('egknservice.index') }}">
+                <i class="la la-archive"></i>
+                <p>Поступившие заявки</p>
+            </a>
           </li>
-          @if( request()->segment(1) == 'process' ||  request()->segment(1) == 'processes' ||  request()->segment(1) == 'processes-edit' || request()->segment(1) == 'template-field-create'  )
-              <li class="nav-item active">
-          @else
-              <li class="nav-item">
-          @endif
-              <a href="{{ route('processes.index') }}">
-                  <i class="la la-gears"></i>
-                  <p>Процессы</p>
-                  <span class="badge badge-count">{{ $processesCount }}</span>
-              </a>
+          <li class="nav-item {{request()->segment(1) == 'process' ? 'active' : request()->segment(1) == 'processes' ? 'active' : request()->segment(1) == 'processes-edit' ? 'active' : request()->segment(1) == 'template-field-create' ? 'active' : ''}}">
+            <a href="{{ route('processes.index') }}">
+                <i class="la la-gears"></i>
+                <p>Процессы</p>
+                <span class="badge badge-count">{{ $processesCount }}</span>
+            </a>
           </li>
-          @if( request()->segment(1) == 'roles' || request()->segment(1) == 'role-edit' )
-              <li class="nav-item active">
-          @else
-              <li class="nav-item">
-          @endif
-              <a href="{{ route('role.index') }}">
-                  <i class="la la-user"></i>
-                  <p>Роли</p>
-                  <span class="badge badge-count">{{ $rolesCount }}</span>
-              </a>
+          <li class="nav-item {{request()->segment(1) == 'roles' ? 'active' : request()->segment(1) == 'role-edit' ? 'active' : ''}}">
+            <a href="{{ route('role.index') }}">
+                <i class="la la-user"></i>
+                <p>Роли</p>
+                <span class="badge badge-count">{{ $rolesCount }}</span>
+            </a>
           </li>
-          @if( request()->segment(1) == 'cities'  )
-              <li class="nav-item active">
-          @else
-              <li class="nav-item">
-          @endif
-              <a href="{{ route('city.index') }}">
-                  <i class="la la-building"></i>
-                  <p>Организации</p>
-                  <span class="badge badge-count">{{ $cityManagementCount }}</span>
-              </a>
+          <li class="nav-item {{request()->segment(1) == 'cities' ? 'active' : ''}}">
+            <a href="{{ route('city.index') }}">
+                <i class="la la-building"></i>
+                <p>Организации</p>
+                <span class="badge badge-count">{{ $cityManagementCount }}</span>
+            </a>
           </li>
-          @if( request()->segment(1) == 'role-register' || request()->segment(1) == 'user-edit'  )
-              <li class="nav-item active">
-          @else
-              <li class="nav-item">
-          @endif
-              <a href="{{ route('user-role.register') }}">
-                  <i class="la la-users"></i>
-                  <p>Пользователи</p>
-                  <span class="badge badge-count">{{ $usersCount }}</span>
-              </a>
+          <li class="nav-item {{request()->segment(1) == 'role-register' ? 'active' : request()->segment(1) == 'user-edit' ? 'active' : ''}}">
+            <a href="{{ route('user-role.register') }}">
+                <i class="la la-users"></i>
+                <p>Пользователи</p>
+                <span class="badge badge-count">{{ $usersCount }}</span>
+            </a>
           </li>
-          @if( request()->segment(1) == 'dictionary')
-              <li class="nav-item active">
-          @else
-              <li class="nav-item">
-          @endif
-              <a href="{{ route('dictionary') }}">
+          <li class="nav-item {{request()->segment(1) == 'dictionary' ? 'active' : ''}}">
+            <a href="{{ route('dictionary') }}">
                 <i class="la la-book"></i>
                 <p>Справочник</p>
                 <span class="badge badge-count">{{ $dictionariesCount }}</span>
-              </a>
+            </a>
           </li>
-          @if( request()->segment(1) == 'logs' )
-              <li class="nav-item active">
-          @else
-              <li class="nav-item">
-          @endif
-              <a href="{{ route('logs') }}">
+          <li class="nav-item {{request()->segment(1) == 'logs' ? 'active' : ''}}">
+            <a href="{{ route('logs') }}">
                 <i class="la la-comment"></i>
                 <p>Логи сервиса</p>
-              </a>
+            </a>
           </li>
-          @if( request()->segment(1) == 'services' || request()->segment(1) == 'index' || request()->segment(1) == 'applications-create')
-              <li class="nav-item active">
-          @else
-              <li class="nav-item">
-          @endif
-              <a href="{{ route('applications.service') }}">
+          <li class="nav-item {{request()->segment(1) == 'services' ? 'active' : request()->segment(1) == 'index' ? 'active' : request()->segment(1) == 'applications-create' ? 'active' : ''}}">
+            <a href="{{ route('applications.service') }}">
                 <i class="la la-list"></i>
                 <p>Все услуги</p>
-              </a>
+            </a>
           </li>
       </ul>
     @else
