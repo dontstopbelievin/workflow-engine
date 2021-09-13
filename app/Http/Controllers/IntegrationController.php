51df94266f031f4ab53f98873ea3cc579099a099
aman <amanbek94@gmail.com>
2021-09-13 18:29:56 +0600

    a

--- app/Http/Controllers/IntegrationController.php
+++ app/Http/Controllers/IntegrationController.php
@@ -32,23 +32,23 @@ class IntegrationController extends Controller
 
     public function test() // HERE!!!
     {
-       // return view('test');
+       return 'asdf';
         // return Carbon::now()->toDateTimeString();
         // return hash('sha512', 'admin@gmail.com123ETO_SOL');
         try {
             $response = array();
             $response['egkn_receive_status'] = ShepRequestSender::send('egkn_receive_status', $_GET);
-            $response['egkn_receive_order'] = ShepRequestSender::send('egkn_receive_order', $_GET);
-            $response['geoportal_egkn_receive_layer'] = ShepRequestSender::send('geoportal_egkn_receive_layer', $_GET);
-            $response['egkn_geoportal_actualization'] = ShepRequestSender::send('egkn_geoportal_actualization', $_GET);
-            $response['eds_temp_files'] = ShepRequestSender::send('eds_temp_files', $_GET);
-            $data = [];
-            $data['correlationId'] = '-1999';
-            $data['messageType'] = '-1999';
-            $data['data'] = ['test' => 'test'];
-            $response['geoportal_pep_async'] = ShepRequestSender::send('geoportal_pep_async', $data);
-            $response['ais_gzk_get_data'] = ShepRequestSender::send('ais_gzk_get_data', $_GET);
-            $response['ais_gzk_get_relevance'] = ShepRequestSender::send('ais_gzk_get_relevance', $_GET);
+            // $response['egkn_receive_order'] = ShepRequestSender::send('egkn_receive_order', $_GET);
+            // $response['geoportal_egkn_receive_layer'] = ShepRequestSender::send('geoportal_egkn_receive_layer', $_GET);
+            // $response['egkn_geoportal_actualization'] = ShepRequestSender::send('egkn_geoportal_actualization', $_GET);
+            // $response['eds_temp_files'] = ShepRequestSender::send('eds_temp_files', $_GET);
+            // $data = [];
+            // $data['correlationId'] = '-1999';
+            // $data['messageType'] = '-1999';
+            // $data['data'] = ['test' => 'test'];
+            // $response['geoportal_pep_async'] = ShepRequestSender::send('geoportal_pep_async', $data);
+            // $response['ais_gzk_get_data'] = ShepRequestSender::send('ais_gzk_get_data', $_GET);
+            // $response['ais_gzk_get_relevance'] = ShepRequestSender::send('ais_gzk_get_relevance', $_GET);
             return view('test')->with('data', $response);
         } catch (\Exception $e) {
             return $e->getMessage();
